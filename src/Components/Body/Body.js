35d410776fdd5333e4c6b260823c03fd4d13210e
Pradeep Saran <pradeepmadasar@gmail.com>
2023-06-17 14:52:27 +0530

    filter changes

--- src/Components/Body/Body.js
+++ src/Components/Body/Body.js
@@ -6,22 +6,26 @@ import resList from '../../Utils/mockData'
 let demorestList = [...resList]
 
 const Body = () => {
-  
+
   const [resList, setReslist] = useState(demorestList)
 
   return (
     <div className='body'>
       <div className='filter-btn'>
-        <button onClick={(e) =>{
-        demorestList =  demorestList.filter(
-        (e)=>e.data.avgRating>4
-       );
-       setReslist(demorestList)
-        console.log( demorestList);
-      }}>Top Rated Restaurant</button>
+        <button onClick={(e) => {
+          const result = demorestList.filter(
+            (e) => e.data.avgRating > 4
+          );
+          setReslist(result)
+          console.log(result);
+        }}>Top Rated Restaurant</button>
+        <button onClick={(e) => {
+          setReslist(demorestList)
+          console.log(demorestList);
+        }}>Reset</button>
       </div>
       <div className='res-container'>
-        { resList.map((e) => (<RestaurantCard key={e.data.id} {...e} />))}
+        {resList.map((e) => (<RestaurantCard key={e.data.id} {...e} />))}
       </div>
     </div>
   )
