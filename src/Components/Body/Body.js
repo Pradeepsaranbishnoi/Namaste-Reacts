2d1ab619ccdb66f8f6375433c64f61d60c963896
Pradeep Saran <pradeepmadasar@gmail.com>
2023-06-17 17:47:15 +0530

    live api

--- src/Components/Body/Body.js
+++ src/Components/Body/Body.js
@@ -1,13 +1,26 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import RestaurantCard from './RestaurantCard'
 import resList from '../../Utils/mockData'
-
+import { ShimmerPostList } from "react-shimmer-effects";
 
 let demorestList = [...resList]
 
 const Body = () => {
 
-  const [resList, setReslist] = useState(demorestList)
+  const [resList, setReslist] = useState("")
+
+  useEffect(()=>{
+    fetchData();
+  },[])
+
+  const fetchData = async () =>{
+    const data = await fetch("https://www.swiggy.com/dapi/restaurants/list/v5?lat=26.2389469&lng=73.02430939999999&page_type=DESKTOP_WEB_LISTING");
+
+    const json = await data.json();
+    console.log(json);
+    setReslist(json?.data?.cards[2]?.data?.data?.cards);
+  };
+
 
   return (
     <div className='body'>
@@ -24,9 +37,18 @@ const Body = () => {
           console.log(demorestList);
         }}>Reset</button>
       </div>
-      <div className='res-container'>
-        {resList.map((e) => (<RestaurantCard key={e.data.id} {...e} />))}
-      </div>
+      { 
+        resList ?
+          <div className='res-container'>
+            {
+              resList.map((e) => (<RestaurantCard key={e.data.id} {...e} />))
+            }
+          </div> : 
+        <>
+        <ShimmerPostList postStyle="STYLE_FOUR" col={4} row={2} gap={30} />
+        </>
+      }
+      
     </div>
   )
 }
--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,19 +1,23 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { LOGO_URL } from '../../Utils/constants'
 
-const Header = () =>{
+const Header = () => {
+  const [btnName, setBtnName] = useState("Login")
   return (
     <div className='header'>
-        <div>
-            <img className='logo' src={LOGO_URL} alt='logo'/>
-        </div>  
-        <div className='nav-items'>
-            <ul>
-                <li>Home</li>
-                <li>About Us</li>
-                <li>Contact Us</li>
-            </ul>
-        </div>    
+      <div>
+        <img className='logo' src={LOGO_URL} alt='logo' />
+      </div>
+      <div className='nav-items'>
+        <ul>
+          <li>Home</li>
+          <li>About Us</li>
+          <li>Contact Us</li>
+          <button onClick={() => {
+            btnName === "Login" ? setBtnName("Logout") : setBtnName("Login")
+          }}>{btnName}</button>
+        </ul>
+      </div>
     </div>
   )
 }
