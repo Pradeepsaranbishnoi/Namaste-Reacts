3ab625d6e728311af471b2e0b246a13bdafd2701
Pradeep Saran <pradeepmadasar@gmail.com>
2023-06-23 17:02:03 +0530

    routes and 404 error

--- src/App.js
+++ src/App.js
@@ -1,14 +1,25 @@
-import Body from "./Components/Body/Body";
-import Header from "./Components/Header/Header";
+import Body from "./Components/Body/Body"
+import Header from "./Components/Header/Header"
+import {
+  BrowserRouter,
+  Route,
+  Routes
+} from "react-router-dom";
+
 
 function App() {
-  
   return (
-    <div className="App">
-      <Header/>
-      <Body/>
-    </div>
-  );
+    <>
+    <Header/>
+    <BrowserRouter>    
+      <Routes>
+        <Route exact path='/' errorElement={<>this is 404 page</>} element={ <Body/>}/> 
+        <Route exact path='/about-us' element={<>This is about us sectoin</>}/>
+        <Route exact path='/contact-us' element={<>This is contact us page</>}/>   
+        <Route path="*" element={"This is the 404 page"} />
+      </Routes>
+    </BrowserRouter>
+    
+    </>)
 }
-
 export default App;
